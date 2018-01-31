ae5079055059936c3036fc5970878cef7c10aea9
Jonas Ahrens <jonas.ahrens@gmail.com>
2018-01-31 20:04:37 +0100

    Fix rebinding of event handlers
    I am not a clever man :( 
    https://i.imgur.com/a4CVG.jpg

--- src/js/pop-state-handler.js
+++ src/js/pop-state-handler.js
@@ -22,7 +22,7 @@ class PopStateHandler {
   addEventHandler () {
     let that = this;
     // $('a.pop').unbind('click');
-    $('a.pop').off('click', 'a.pop');
+    $(document).off('click', 'a.pop');
     $(document).on('click', 'a.pop', function (e) {
       let href = $(this).attr('href');
       history.pushState(null, null, href);
